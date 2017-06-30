0d3f259c7384d7c8c47a5f1728888ac178b5ec41
calvin cushman <calvincushman@gmail.com>
2017-06-30 15:40:00 -0700

    fix broken get request

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     var city  = $('#location').val();
     $('#location').val("");
     $('.showDoctors').text("Finding doctors in " + city + ".");
-    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ city +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&apikey=' + user_key, function(response) {
+    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ city +'&user_key=' + user_key, function(response) {
       console.log(response);
     });
   });
