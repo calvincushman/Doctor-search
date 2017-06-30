79038073c52a2486ee3e4e3d05f4e58c5f309a7f
calvin cushman <calvincushman@gmail.com>
2017-06-30 16:01:51 -0700

    move get request to back end, build constuctor to output to the front end

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -1,13 +1,14 @@
-var user_key = require('./../.env').apikey;
+var Doctor = require('./../js/scripts.js').doctorModule;
 
-
-$(document).ready(function() {
+$(function() {
+  var currentDoctorObject = new Doctor();
   $('#doctor-location').click(function() {
-    var city  = $('#location').val();
-    $('#location').val("");
-    $('.showDoctors').text("Finding doctors in " + city + ".");
-    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ city +'&user_key=' + user_key, function(response) {
-      console.log(response);
+      var city  = $('#location').val();
+      $('#location').val("");
+      $('.showDoctors').text("Finding doctors in " + city + ".");
     });
   });
 });
+
+
+currentDoctorObject.getDoctor();
