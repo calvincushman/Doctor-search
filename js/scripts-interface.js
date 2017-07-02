01acacb5c98c114844d42039a324a324404937bf
Calvin Cushman <Guest@Epicodus-2A.local>
2017-07-02 15:47:17 -0700

    get displayDoctor function behaving properly displaying doctor name and bio

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -1,12 +1,16 @@
-var user_key = require('./../.env').apiKey;
+var Doctor = require('./../js/scripts.js').doctorModule;
 
-$(document).ready(function() {
+var displayDoctor = function(city, doctorName, doctorBio) {
+  $('.showCity').text("doctors in " + city + ".")
+  $('.showName').text(doctorName)
+  $('.showBio').text(doctorBio);
+}
+
+$(function() {
+  var currentDoctorObject = new Doctor();
   $('#doctor-location').click(function() {
     var city  = $('#location').val();
     $('#location').val("");
-    $('.showDoctors').text("Finding doctors in " + city + ".");
-    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ city +'&user_key=' + user_key, function(response) {
-      console.log(response);
-    });
+    currentDoctorObject.getDoctor(city, displayDoctor);
   });
 });
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
+var user_key = require('./../.env').apiKey;
 
+function Doctor() {
+}
 
-var Doctor 
+Doctor.prototype.getDoctor = function(city, displayDoctor) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ city +'&user_key=' + user_key)
+  .then(function(response) {
+    console.log(response);
+    displayDoctor(city, response.data["0"].profile.first_name + " " + response.data["0"].profile.last_name, response.data["0"].profile.bio);
+  }).fail(function(error) {
+    $('.showDoctor').text(error.responseJSON.message);
+  })
+}
+
+exports.doctorModule = Doctor;
+
+// displayDoctor(response.data["0"].profile.first_name + " " + response.data["0"].profile.last_name);
+// displayDoctor(response.data["0"].profile.bio);
